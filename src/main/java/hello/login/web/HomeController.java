04cdf42b8c06d340cd48cca82b835b5938ac06a1
chocolaggibbiddori <didrkd684@naver.com>
2022-12-12 21:40:21 +0900

    feat : LoninMemberArgumentResolver - 생성
    1. HandlerMethodArgumentResolver 구현
    2. supportsParameter이 true면 resolveArgument 실행
    3. WebConfig에 addArgumentResolvers로 등록
    4. @Login 어노테이션 사용

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,16 +2,13 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.seesion.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -79,7 +76,7 @@ public class HomeController {
         return "loginHome";
     }*/
 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
         if (loginMember == null) {
@@ -88,6 +85,17 @@ public class HomeController {
 
         model.addAttribute("member", loginMember);
 
+        return "loginHome";
+    }*/
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
